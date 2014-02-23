19930daf6c6ff61925b3d05a13469ff59d8ae9b7
MAMISHO <mamisho@hotmail.es>
2014-02-23 02:48:48 +0100

    Ferri alpha 0.0.1
    implementada el relleno de coches y el método de relleno de ferries
    esta en alpa, hay que implementar de una mejor forma

--- alg2/epd2/ferry/src/Ferry.java
+++ alg2/epd2/ferry/src/Ferry.java
@@ -16,11 +16,12 @@ import queues.QueueADT;
  */
 public class Ferry {
     CircularArrayQueue<ArrayQueue> parking;
-    ArrayQueue fila;
+    ArrayQueue<ArrayQueue> ferries;
     
-    public Ferry(){
+    public Ferry(){//El parking inicia lleno.
         parking=new CircularArrayQueue<ArrayQueue>();
-        
+        ferries=new ArrayQueue<ArrayQueue>();
+        ArrayQueue fila;
         for (int i = 0; i < 5; i++) {
             fila=new ArrayQueue();
             
@@ -42,12 +43,33 @@ public class Ferry {
     }
    
     public void rellerferry(){
-        
+        ArrayQueue ferry= new ArrayQueue();
+        while(ferry.size()<220){
+            //aqui hay que sacar una fila de coches, y luego meterlas en una cola auxiliar, luego meterla y controlar tamaños.
+            //por eso esta implementación falla, arreglar.
+            ferry.enqueue(parking.dequeue().dequeue());
+        }
+        ferries.enqueue(ferry);
     }
     
     public void imprime(){
         QueueADT q=new ArrayQueue();
-       
+        int cont=0;
+        while(parking.size()>0){
+            ArrayQueue colaAux=new ArrayQueue();
+            colaAux=parking.dequeue();
+            System.out.println("Se a desencolado la cola "+cont);
+            while(colaAux.size()>0){
+                System.out.println(colaAux.dequeue());
+            }
+            cont++;
+        }
+    }
+    
+    public void imprimeFerry(){
+        while(ferries.size()>0){
+            System.out.println(ferries.dequeue().dequeue());
+        }
     }
     
 }
--- alg2/epd2/ferry/src/main.java
+++ alg2/epd2/ferry/src/main.java
@@ -14,7 +14,10 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ferry f=new Ferry();
+        //f.imprime();
+        f.rellerferry();
+        f.imprimeFerry();
     }
     
 }
