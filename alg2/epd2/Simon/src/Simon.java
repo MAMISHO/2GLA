873c9ab190c62b7cdc4ccfb29749b581ed32cd3a
MAMISHO <mamisho@hotmail.es>
2014-03-02 01:45:47 +0100

    Simon
    Creado el proyecto, pero aun no esta implementado, ya que estoy
    planteando los niveles de juego

---
+++ alg2/epd2/Simon/src/Simon.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MAMISHO
+ */
+public class Simon {
+    //creamos 2 tipos de modalidades,
+    //1- el modo normal
+    //2- nievl ingeniero, consiste en que el orden hay que hacerlo a la inversa.
+    //explicamos que es mejor hacerlo con colas, pero queremos complicarnos la vida y hacerlo con pilas
+    
+    
+}
