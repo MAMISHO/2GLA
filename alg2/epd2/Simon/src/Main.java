d24faf875cdc06df13378d6257a18603054dc8b2
MAMISHO <mamisho@hotmail.es>
2014-03-03 17:34:45 +0100

    Esqueleto de Simon
    Esta el esqueleto y un poco de la implementación de el ejercicio que
    estamos panteando como Simon
    
    Hay que poner los comentarios en el código y hacer el documento

--- alg2/epd2/Simon/src/Main.java
+++ alg2/epd2/Simon/src/Main.java
@@ -16,7 +16,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Simon s=new Simon();
+        s.iniciarJuego();
     }
     
 }
--- alg2/epd2/Simon/src/Simon.java
+++ alg2/epd2/Simon/src/Simon.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 
+
+import java.util.Scanner;
+import jss2.*;
+import stacks.*;
+
 /**
  *
  * @author MAMISHO
@@ -14,5 +19,98 @@ public class Simon {
     //2- nievl ingeniero, consiste en que el orden hay que hacerlo a la inversa.
     //explicamos que es mejor hacerlo con colas, pero queremos complicarnos la vida y hacerlo con pilas
     
+    private StackADT pila;
+    private DoubleIndexedList  lista;
+    Scanner sc;
     
+    public Simon(){
+        pila=new ArrayStack();
+        lista=new DoubleIndexedList();
+        sc = new Scanner(System.in);
+    }
+    
+    public void iniciarJuego(){
+        boolean falla=false;
+        int resp=0;
+        secuenciaInicial();
+        while(!falla){
+            resp=usuarioIntroduce();
+            falla=comprobarEntrada(resp);
+            if(!falla){
+                nuevaSecuencia();
+            }else{
+                System.out.println("¡Game over!");
+            }
+        }
+    }
+    
+    private void secuenciaInicial(){
+        int color=nuevoColor();
+        pila.push(color);
+        muestraColor(color);
+        
+        color=nuevoColor();
+        pila.push(color);
+        muestraColor(color);
+    }
+    
+    private void nuevaSecuencia(){
+        pila.push(nuevoColor());
+        StackADT pAux=pila;
+        while(!pAux.isEmpty()){
+            lista.add(pAux.pop());
+        }
+        DoubleIterator it= (DoubleIterator)lista.iterator();
+        while(it.hasNext()){
+            int aux=(int)it.next();
+            muestraColor(aux);
+        }
+    }
+    
+    private boolean comprobarEntrada(int color){
+        return false;//true si se equivoca else si esta bien
+    }
+    
+    private int nuevoColor(){
+        int nuevoColor=(int) Math.floor(Math.random()*5+1);
+        int ultimoColor=0;
+        if(!pila.isEmpty()){
+            ultimoColor=(int)pila.pop();
+        }
+        
+        while(nuevoColor==ultimoColor){
+            nuevoColor=(int) Math.floor(Math.random()*5+1);
+        }
+        return nuevoColor;
+    }
+    
+    private void muestraColor(int color){
+        switch(color){
+            case 1:
+                System.out.println("Amarillo");
+            break;
+            case 2:
+                System.out.println("Azul");
+            break;
+            case 3:
+                System.out.println("Rojo");
+            break;
+            case 4:
+                System.out.println("Verde");
+            break;
+        }
+    }
+    
+    private int usuarioIntroduce(){
+        int resp=0;
+        do{
+            System.out.println("introduce el valor de la secuencia");
+            resp=sc.nextInt();
+            if(resp<1 && resp>4){
+                System.out.println("Color invalido");
+            }
+        }while(resp<1 && resp>4);
+        
+        return resp;
+    }
 }
